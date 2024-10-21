b791d1898dc61d3ea10de05f7819e81a23013091
Douae sb <douae.sb411@gmail.com>
2024-10-21 10:12:46 +0100

    logger slf4j

---
+++ src/main/java/game/gameDev/Main.java
@@ -0,0 +1,15 @@
+package game.gameDev;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    public static void main(String[] args)  {
+
+         final Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("logger initialized.");
+
+
+
+    }
+}
