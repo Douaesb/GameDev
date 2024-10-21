6cdc2d65a9bc2bec82967158243a85896f65bb83
Douae sb <douae.sb411@example.com>
2024-10-21 14:06:06 +0100

    applicationContext persistence config

--- src/main/java/com/gameDev/service/impl/TournamentServiceImpl.java
+++ src/main/java/com/gameDev/service/impl/TournamentServiceImpl.java
@@ -1,4 +1,12 @@
 package com.gameDev.service.impl;
 
+import com.gameDev.dao.TournamentDao;
+
 public class TournamentServiceImpl {
+    private TournamentDao tournamentDao;
+
+    public void setTournamentDao(TournamentDao tournamentDao) {
+        this.tournamentDao = tournamentDao;
+    }
+
 }
