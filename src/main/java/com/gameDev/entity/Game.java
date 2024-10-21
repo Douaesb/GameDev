87b76ac6e08803fe8eb45902faf578b0fda40550
Douae sb <douae.sb411@example.com>
2024-10-21 16:18:11 +0100

    GAM-16-game-entity

--- src/main/java/com/gameDev/entity/Game.java
+++ src/main/java/com/gameDev/entity/Game.java
@@ -1,4 +1,72 @@
 package com.gameDev.entity;
 
+import javax.persistence.*;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "games")
 public class Game {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotNull(message = "game cannot be null")
+    @Size(min = 2, max = 100, message = "Game name must be between 2 and 100 characters." )
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @NotNull(message = "Difficulty cannot be null")
+    @Size(min = 3, max = 50, message = "Difficulty must be between 3 and 50 characters")
+    @Column(name = "difficulty", nullable = false)
+    private String difficulty;
+
+    @NotNull(message = "Match average duration cannot be null")
+    @Min(value = 0, message = "Match average duration must be positive")
+    @Column(name = "match_avg_duration", nullable = false)
+    private Double matchAvgDuration;
+
+
+    // Constructors
+    public Game() {
+    }
+
+    public Game(String name, String difficulty, Double matchAvgDuration) {
+        this.name = name;
+        this.difficulty = difficulty;
+        this.matchAvgDuration = matchAvgDuration;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(String difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public Double getMatchAvgDuration() {
+        return matchAvgDuration;
+    }
+
+    public void setMatchAvgDuration(Double matchAvgDuration) {
+        this.matchAvgDuration = matchAvgDuration;
+    }
 }
